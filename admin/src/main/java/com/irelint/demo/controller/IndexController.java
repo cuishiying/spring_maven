074696290a605a939c297a96b7c289e5d539b3b4
崔世英 <cuishiying@irelint.com>
2017-02-25 19:02:59 +0800

    添加Controller测试接口

--- admin/src/main/java/com/irelint/demo/controller/IndexController.java
+++ admin/src/main/java/com/irelint/demo/controller/IndexController.java
@@ -1,8 +1,8 @@
 package com.irelint.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.irelint.demo.base.AjaxResponse;
+import com.irelint.demo.dto.User;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -11,10 +11,67 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 @RequestMapping("/")
 public class IndexController {
-    @RequestMapping(path = "/pager",method = RequestMethod.GET)
-    public ModelAndView index(){
+
+    /**
+     * 返回数据到模板页面
+     *
+     * @return
+     */
+    @RequestMapping(path = "/pager", method = RequestMethod.GET)
+    public ModelAndView index() {
         ModelAndView model = new ModelAndView("index");
-        model.addObject("name","test");
+        model.addObject("name", "test");
         return model;
+    }
+
+    /**
+     * 接口形式，返回json
+     *
+     * @return
+     */
+    @RequestMapping(path = "/json", method = RequestMethod.GET)
+    public Object testJson() {
+        User user = new User();
+        user.setName("test");
+        user.setSex("man");
+        return user;
+    }
+
+    /**
+     * 从客户端提交参数
+     */
+    @RequestMapping(value = "uploadParams", method = RequestMethod.GET)
+    public Object uploadParams(String name,String sex) {
+        User userInfo = new User();
+        userInfo.setName(name);
+        userInfo.setSex(sex);
+        return AjaxResponse.success();
+    }
+    /**
+     * 从客户端提交参数,rest风格
+     */
+    @RequestMapping(value = "uploadParams/{name}", method = RequestMethod.GET)
+    public Object restParams(@PathVariable String name) {
+        User userInfo = new User();
+        userInfo.setName(name);
+        userInfo.setSex("femail");
+        return AjaxResponse.success();
+    }
+
+    /**
+     * 从客户端提交json
+     */
+    @RequestMapping(value = "uploadJson",method = RequestMethod.POST)
+    public Object uploadJson(@RequestBody User userInfo){
+        System.out.print(userInfo.toString());
+        return AjaxResponse.success();
+    }
+    /**
+     * 从客户端提交序列化对象
+     */
+    @RequestMapping(value = "uploadObj",method = RequestMethod.POST)
+    public Object uploadObj(User user){
+        System.out.print(user.getName());
+        return AjaxResponse.success();
     }
 }
